a02708dabcf5cbbadff24ac615f6a4a81d6a6524
AlonGolembo <alon.golembo@gmail.com>
2026-05-25 20:10:26 +0300

    Made GarageLogic a dll and referenced ConsoleUI to it

---
+++ Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ex03.GarageLogic;
+
+namespace Ex03.ConsoleUI
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
