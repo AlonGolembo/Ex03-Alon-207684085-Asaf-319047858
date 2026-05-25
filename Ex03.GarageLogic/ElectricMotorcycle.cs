e8a1394656cad724b7b6fa2b3dfafe6374c4b14e
AlonGolembo <alon.golembo@gmail.com>
2026-05-25 22:54:52 +0300

    Added the different vehicle classes with the names according to the VehiclesCreator class only so the project can compile

---
+++ Ex03.GarageLogic/ElectricMotorcycle.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex03.GarageLogic
+{
+    internal class ElectricMotorcycle : Vehicle
+    {
+        private string i_LicenseID;
+        private string i_ModelName;
+
+        public ElectricMotorcycle(string i_LicenseID, string i_ModelName)
+        {
+            this.i_LicenseID = i_LicenseID;
+            this.i_ModelName = i_ModelName;
+        }
+    }
+}
---
+++ Ex03.GarageLogic/FuelMotorcycle.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex03.GarageLogic
+{
+    internal class FuelMotorcycle : Vehicle
+    {
+        private string i_LicenseID;
+        private string i_ModelName;
+
+        public FuelMotorcycle(string i_LicenseID, string i_ModelName)
+        {
+            this.i_LicenseID = i_LicenseID;
+            this.i_ModelName = i_ModelName;
+        }
+    }
+}
