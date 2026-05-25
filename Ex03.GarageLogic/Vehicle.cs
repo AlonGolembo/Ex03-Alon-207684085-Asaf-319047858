c2caa35b4ec18ae000f6749aafede788aca1223a
AlonGolembo <alon.golembo@gmail.com>
2026-05-26 00:28:13 +0300

    Make Vehicle class abstract and change it's datamember to be a PowerSource type

--- Ex03.GarageLogic/Vehicle.cs
+++ Ex03.GarageLogic/Vehicle.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace Ex03.GarageLogic
 {
-    public class Vehicle
+    public abstract class Vehicle
     {
         private string m_ModelName;
         private string m_LicenseNumber;
-        private float m_EnergyPercent;
+        public PowerSource Engine { get; set; }
         private List<Wheel> m_Wheels;
 
         public string ModelName
@@ -45,24 +45,11 @@ namespace Ex03.GarageLogic
                 }
             }
         }
-
-        public float EnergyPercent
-        {
-            get { return m_EnergyPercent; }
-            set
-            {
-                if(value < 0)
-                {
-                    throw new ArgumentOutOfRangeException("Energy percent can't be negative!");
-                }
-            }
-        }
-
         public Vehicle(string i_ModelName, string i_LicenseNumber)
         {
             m_ModelName = i_ModelName;
             m_LicenseNumber = i_LicenseNumber;
-            m_EnergyPercent = 100;
+            Engine = new PowerSource();
             m_Wheels = new List<Wheel>();
         }
     }    
