d8fd045ae83a053bdb639fdefd616326ff1890c5
AlonGolembo <alon.golembo@gmail.com>
2026-05-26 18:06:22 +0300

    Created eFuelType enum and Fuel properties

--- Ex03.GarageLogic/Fuel.cs
+++ Ex03.GarageLogic/Fuel.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace Ex03.GarageLogic
 {
-    internal class Fuel
+    public class Fuel : PowerSource
     {
+        public eFuelType FuelType { get; set; }
+        private float m_CurrentFuelAmount;
+        private float m_MaxFuelAmount;
+
+        public float CurrentFuelAmount
+        {
+            get { return m_CurrentFuelAmount; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("The current fuel amount can't be negative!");
+                }
+                else if (value > m_MaxFuelAmount)
+                {
+                    throw new ArgumentOutOfRangeException("The current fuel amount can't be higher than max fuel amount!");
+                }
+                else
+                {
+                    m_CurrentFuelAmount = value;
+                }
+            }
+        }
+
+        public float MaxFuelAmount
+        {
+            get { return m_MaxFuelAmount; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("The max fuel amount can't be negative!");
+                }
+                else
+                {
+                    m_MaxFuelAmount = value;
+                }
+            }
+        }
     }
 }
---
+++ Ex03.GarageLogic/eFuelType.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex03.GarageLogic
+{
+    public enum eFuelType
+    {
+        Soler = 0,
+        Octan95,
+        Octan96,
+        Octan98
+    }
+}
