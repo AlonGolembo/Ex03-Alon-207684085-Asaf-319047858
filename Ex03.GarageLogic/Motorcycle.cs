c2d5ff475b2b4ee0e50c20541c9ece97e0d93cb4
AlonGolembo <alon.golembo@gmail.com>
2026-05-26 23:01:30 +0300

    Create Motorcycle class

---
+++ Ex03.GarageLogic/Motorcycle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class Motorcycle : Vehicle
+    {
+        public eDrivingLicenceCategory DrivingLicenceCategory { get; set; }
+        private int m_EngineCapacity;
+
+        public int EngineCapacity
+        {
+            get { return m_EngineCapacity; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Engine capacity can't be negative!");
+                }
+                else
+                {
+                    m_EngineCapacity = value;
+                }
+            }
+        }
+    }
+}
