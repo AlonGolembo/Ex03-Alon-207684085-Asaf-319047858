9b640ec2c3ef1b362165fd9e80909b31f805a982
Asaf Ezrai <asafez@mta.ac.il>
2026-05-26 21:51:30 +0300

    Handled the Wheel class, and added eDrivingLicenceCategory and ePaint.

--- Ex03.GarageLogic/Wheel.cs
+++ Ex03.GarageLogic/Wheel.cs
@@ -1,7 +1,72 @@
-﻿namespace Ex03.GarageLogic
+﻿using System;
+
+namespace Ex03.GarageLogic
 {
     public class Wheel
     {
+        private string m_ManufacturerName;
+        private float m_CurrentAirPressure;
+        private float m_MaxAirPressure;
+
+        public string ManufacturerName
+        {
+            get { return m_ManufacturerName; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    m_ManufacturerName = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException("Manufacturer name can't be empty!");
+                }
+            }
+        }
+
+        public float CurrentAirPressure
+        {
+            get { return m_CurrentAirPressure; }
+            set
+            {
+                if (value >= 0 && value <= MaxAirPressure)
+                {
+                    m_CurrentAirPressure = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Current amount of air must be positive and not exceed max air pressure!");
+                }
+            }
+        }
+
+        public float MaxAirPressure
+        {
+            get { return m_MaxAirPressure; }
+            set
+            {
+                if (value > 0)
+                {
+                    m_MaxAirPressure = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Max mount of air to for a wheel must be positive and not zero or below.");
+                }
+            }
+        }
+
+        public void Inflate(float i_AirToAdd)
+        {
+            if (i_AirToAdd > 0 && CurrentAirPressure + i_AirToAdd <= MaxAirPressure)
+            {
+                CurrentAirPressure += i_AirToAdd;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException( "Amount of air to add must be positive and not exceed max air pressure!");
+            }
+        }
 
     }
 }
