5cefe8ca91572509842d9334c407e0debf99fd79
AlonGolembo <alon.golembo@gmail.com>
2026-05-27 19:59:16 +0300

    Create ElectricMotorcycle constructor

--- Ex03.GarageLogic/ElectricMotorcycle.cs
+++ Ex03.GarageLogic/ElectricMotorcycle.cs
@@ -2,13 +2,10 @@
 {
     internal class ElectricMotorcycle : Vehicle
     {
-        private string i_LicenseID;
-        private string i_ModelName;
-
-        public ElectricMotorcycle(string i_LicenseID, string i_ModelName)
+        public ElectricMotorcycle(string i_LicenseID, string i_ModelName) : base(i_LicenseID, i_ModelName)
         {
-            this.i_LicenseID = i_LicenseID;
-            this.i_ModelName = i_ModelName;
+            this.InitializeWheelsList(eNumberOfWheels.Two, (float)30);
+            this.Engine = new ElectricEngine((float)3.0);
         }
     }
 }
