1e9b14f426745640b2c4cb1d0c44faa8e4df3b24
Asaf Ezrai <asafez@mta.ac.il>
2026-05-31 15:40:45 +0300

    Added GetVehicle(string i_LicenseID) to VehicleHandler.

--- Ex03.GarageLogic/VehicleHandler.cs
+++ Ex03.GarageLogic/VehicleHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,89 +9,126 @@ namespace Ex03.GarageLogic
 {
     public class VehicleHandler
     {
+        private readonly Garage m_Garage;
+        private readonly Dictionary<string, RegisteredVehicle> m_VehiclesInGarage;
 
-        private readonly RegisteredVehicle m_RegisteredVehicle;
-        private eVehicleState m_VehicleState;
+
+        // private readonly RegisteredVehicle m_RegisteredVehicle;
+        //private eVehicleState m_VehicleState;
 
         //Add GetVehicle method to return
         //the registered vehicle in case
         //we need to check if it already exists in the garage or not
 
 
-        //Add
+      
+         
 
-        public VehicleHandler(RegisteredVehicle i_RegisteredVehicle)
+        public Garage Garage
         {
-            m_RegisteredVehicle = i_RegisteredVehicle;
-            m_VehicleState = eVehicleState.UnderRepair; 
+            get { return m_Garage; }
+        }   
+
+        public Dictionary<string, RegisteredVehicle> VehiclesInGarage
+        {
+            get { return m_VehiclesInGarage; }
         }
-
-
-        public RegisteredVehicle RegisteredVehicle
+        public void InsertToGarage(RegisteredVehicle i_registeredVehicle)
         {
-            get { return m_RegisteredVehicle; }
-        }
-        public eVehicleState VehicleState
-        {
-            get { return m_VehicleState; }
-            set { m_VehicleState = value; }
-        }
-
-
-
-        public void InflateWheelsToMax()
-        {
-            List<Wheel> vehicleWheels = m_RegisteredVehicle.Vehicle.Wheels;
-            foreach (Wheel wheel in vehicleWheels)
+            string licenseID = i_registeredVehicle.Vehicle.LicenseID;
+            if (!m_VehiclesInGarage.ContainsKey(licenseID))
             {
-                float airToAdd = wheel.MaxAirPressure - wheel.CurrentAirPressure;
-
-                if (airToAdd > 0)
-                {
-                    wheel.Inflate(airToAdd); 
-                }
-            }
-        }
-
-
-        public void FuelVehicle(eFuelType i_FuelType, float i_AmountToFuel)
-        {
-            if (m_RegisteredVehicle.Vehicle.Engine is FuelEngine fuelEngine)
-            {
-
-                fuelEngine.Refuel(i_AmountToFuel, i_FuelType);
+                m_VehiclesInGarage.Add(licenseID, i_registeredVehicle);
             }
             else
             {
-
-                throw new ArgumentException("Error: Cannot fuel an electric vehicle!");
+                m_VehiclesInGarage[licenseID].VehicleState = eVehicleState.UnderRepair;
             }
         }
-
-       
-        public void ChargeVehicle(float i_MinutesToCharge)
+        public RegisteredVehicle GetVehicle(string i_LicenseID)
         {
-            if (m_RegisteredVehicle.Vehicle.Engine is ElectricEngine electricEngine)
+            if (m_VehiclesInGarage.ContainsKey(i_LicenseID))
             {
-                float hoursToCharge = i_MinutesToCharge / 60f;
-                electricEngine.ChargeBattery(hoursToCharge);
+                return m_VehiclesInGarage[i_LicenseID];
             }
             else
             {
-
-                throw new ArgumentException("Error: Cannot charge a fuel-based vehicle!");
+                return null;
             }
         }
 
-        
-        public override string ToString()
-        {
-            return string.Format(
-                "{0}\nStatue in Garage: {1}",
-                m_RegisteredVehicle.ToString(),
-                m_VehicleState.ToString()
-            );
-        }
+        // public VehicleHandler(RegisteredVehicle i_RegisteredVehicle)
+        //{
+        //  m_RegisteredVehicle = i_RegisteredVehicle;
+        //m_VehicleState = eVehicleState.UnderRepair; 
+        // }
+
+
+        //public RegisteredVehicle RegisteredVehicle
+        //{
+        //    get { return m_RegisteredVehicle; }
+        //}
+        //public eVehicleState VehicleState
+        //{
+        //    get { return m_VehicleState; }
+        //    set { m_VehicleState = value; }
+        //}
+
+
+
+        //public void InflateWheelsToMax() -->> Garage class
+        //{
+        //    List<Wheel> vehicleWheels = m_RegisteredVehicle.Vehicle.Wheels;
+        //    foreach (Wheel wheel in vehicleWheels)
+        //    {
+        //        float airToAdd = wheel.MaxAirPressure - wheel.CurrentAirPressure;
+
+        //        if (airToAdd > 0)
+        //        {
+        //            wheel.Inflate(airToAdd); 
+        //        }
+        //    }
+        //}
+
+
+        //public void FuelVehicle(eFuelType i_FuelType, float i_AmountToFuel) -->> Garage class
+        //{
+        //    if (m_RegisteredVehicle.Vehicle.Engine is FuelEngine fuelEngine)
+        //    {
+
+        //        fuelEngine.Refuel(i_AmountToFuel, i_FuelType);
+        //    }
+        //    else
+        //    {
+
+        //        throw new ArgumentException("Error: Cannot fuel an electric vehicle!");
+        //    }
+        //}
+
+
+        //public void ChargeVehicle(float i_MinutesToCharge) -->> Garrage class
+        //{
+        //    if (m_RegisteredVehicle.Vehicle.Engine is ElectricEngine electricEngine)
+        //    {
+        //        float hoursToCharge = i_MinutesToCharge / 60f;
+        //        electricEngine.ChargeBattery(hoursToCharge);
+        //    }
+        //    else
+        //    {
+
+        //        throw new ArgumentException("Error: Cannot charge a fuel-based vehicle!");
+        //    }
+        //}
+
+
+        //public override string ToString() -->> Refraze the to string to methods that prints after filtering the vehicles in the garage by their state and type, and add the vehicle state to the printout
+        //{
+        //    return string.Format(
+        //        "{0}\nStatue in Garage: {1}",
+        //        m_RegisteredVehicle.ToString(),
+        //        m_VehicleState.ToString()
+        //    );
+        //}
     }
 }
 
