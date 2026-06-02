34c440adef4585a8b7302f40d23682fe8a6cd9a9
AlonGolembo <alon.golembo@gmail.com>
2026-06-02 22:08:59 +0300

    Fix FuelMotorcycle inheritance and constructor

--- Ex03.GarageLogic/FuelMotorcycle.cs
+++ Ex03.GarageLogic/FuelMotorcycle.cs
@@ -1,9 +1,12 @@
 ﻿namespace Ex03.GarageLogic
 {
-    public class FuelMotorcycle : Vehicle
+    public class FuelMotorcycle : Motorcycle
     {
 
-        public FuelMotorcycle(string i_LicenseID, string i_ModelName) : base(i_LicenseID, i_ModelName)
+        public FuelMotorcycle(string i_LicenseID,
+                              string i_ModelName,
+                              int i_EngineCapacity,
+                              eDrivingLicenceCategory i_DrivingLicenceCategory) : base(i_LicenseID, i_ModelName, i_EngineCapacity, i_DrivingLicenceCategory)
         {
             this.InitializeWheelsList(eNumberOfWheels.Two, (float)30);
             this.Engine = new FuelEngine(eFuelType.Octan98, (float)1.0);
