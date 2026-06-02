07ca7c3d8494110903138fd8b78ff3719d78c2ea
AlonGolembo <alon.golembo@gmail.com>
2026-06-02 23:03:34 +0300

    Add ElectricMotorcycle default constructor

--- Ex03.GarageLogic/ElectricMotorcycle.cs
+++ Ex03.GarageLogic/ElectricMotorcycle.cs
@@ -3,6 +3,13 @@
     public class ElectricMotorcycle : Motorcycle
     {
         public ElectricMotorcycle(string i_LicenseID,
+                                  string i_ModelName) : base(i_LicenseID, i_ModelName)
+        {
+            this.InitializeWheelsList(eNumberOfWheels.Two, (float)30);
+            this.Engine = new ElectricEngine((float)3.0);
+        }
+
+        public ElectricMotorcycle(string i_LicenseID,
                                   string i_ModelName,
                                   int i_EngineCapacity,
                                   eDrivingLicenceCategory i_DrivingLicenceCategory) : base(i_LicenseID, i_ModelName, i_EngineCapacity, i_DrivingLicenceCategory)
