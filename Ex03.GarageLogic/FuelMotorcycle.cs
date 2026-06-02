b5c2fb8113c334148fcd19235401d43900021434
AlonGolembo <alon.golembo@gmail.com>
2026-06-02 23:02:43 +0300

    Add FuelMotorcycle default constructor

--- Ex03.GarageLogic/FuelMotorcycle.cs
+++ Ex03.GarageLogic/FuelMotorcycle.cs
@@ -2,7 +2,12 @@
 {
     public class FuelMotorcycle : Motorcycle
     {
-
+        public FuelMotorcycle(string i_LicenseID,
+                              string i_ModelName) : base(i_LicenseID, i_ModelName)
+        {
+            this.InitializeWheelsList(eNumberOfWheels.Two, (float)30);
+            this.Engine = new FuelEngine(eFuelType.Octan98, (float)1.0);
+        }
         public FuelMotorcycle(string i_LicenseID,
                               string i_ModelName,
                               int i_EngineCapacity,
