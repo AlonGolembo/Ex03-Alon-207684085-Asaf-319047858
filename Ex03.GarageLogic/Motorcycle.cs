1c7d274eae335e0c27644c08decb64f7b0c4ff57
AlonGolembo <alon.golembo@gmail.com>
2026-06-02 22:06:20 +0300

    Fix Motorcycle constructor and remove the empty contructor

--- Ex03.GarageLogic/Motorcycle.cs
+++ Ex03.GarageLogic/Motorcycle.cs
@@ -11,9 +11,10 @@ namespace Ex03.GarageLogic
         public eDrivingLicenceCategory DrivingLicenceCategory { get; set; }
         private int m_EngineCapacity;
 
-        public Motorcycle (string i_LicenseID, string i_ModelName,int i_EngineCapacity) : base(i_LicenseID, i_ModelName)
+        public Motorcycle (string i_LicenseID, string i_ModelName,int i_EngineCapacity, eDrivingLicenceCategory i_DrivingLicenceCategory) : base(i_LicenseID, i_ModelName)
         {
             EngineCapacity = i_EngineCapacity;
+            DrivingLicenceCategory = i_DrivingLicenceCategory;
         }
 
         public int EngineCapacity
@@ -31,8 +32,5 @@ namespace Ex03.GarageLogic
                 }
             }
         }
-
-        public Motorcycle(string i_LicenseNumber,
-                          string i_ModelName) : base(i_LicenseNumber, i_ModelName) { }
     }
 }
